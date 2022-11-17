66d98e42320329e64faac9b6133602f1050642fb
illuhs <illuhs@yandex.ru>
2022-11-17 21:33:02 +0300

    add routes and repositories

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'  //import express, {Request, Response, Router}
+import {blogsRouter} from "./routes/blogs-router";
+
+export const app = express();
+const port = process.env.PORT || 3000;
+const jsonBody = express.json();
+app.use(jsonBody);
+
+app.use('/blogs', blogsRouter);
