64bfced043cca7a0b30557d2818030822ab4febd
illuhs <illuhs@yandex.ru>
2022-11-19 16:55:15 +0300

    add posts validation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express, {NextFunction, Request, Response} from 'express'  //import express, {Request, Response, Router}
 import {blogsRouter} from "./routes/blogs-router";
+import {mainRouter} from "./routes/main-router";
 
 export const app = express();
 const port = process.env.PORT || 3000;
 const jsonBody = express.json();
 app.use(jsonBody);
-
+app.use('/', mainRouter)
 app.use('/blogs', blogsRouter);
 
 app.listen(port, () => {
