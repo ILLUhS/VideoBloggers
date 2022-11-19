a362bb587250daefb8deb9fcc475d55c3cf558b4
illuhs <illuhs@yandex.ru>
2022-11-19 12:49:53 +0300

    add methods of blogs repository

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -7,15 +7,46 @@ type BlogsType = {
 type BlogsRepositoryType = {
     blogs: BlogsType[];
 };
-const BlogsRepository: BlogsRepositoryType = {
+const blogsRepositoryDb: BlogsRepositoryType = {
     blogs: []
 };
 
 export const blogsRepository = {       //объект с методами управления данными
     returnAllBlogs() {
-        return BlogsRepository.blogs;
+        return blogsRepositoryDb.blogs;
     },
     findBlogById(id: number) {
-
+        return blogsRepositoryDb.blogs.find(b => b.id === id); //array || undefined
+    },
+    deleteBlogByTd(id: number) {
+        for(let i = 0; i < blogsRepositoryDb.blogs.length; i++) {
+            if(blogsRepositoryDb.blogs[i].id === id) {
+                blogsRepositoryDb.blogs.splice(i, 1);
+                return true;
+            }
+        }
+        return false
+    },
+    createBlog(name: string, description: string, websiteUrl: string) {
+        const newBlog = {
+            id: Number(new Date()),
+            name: name,
+            description: description,
+            websiteUrl: websiteUrl
+        };
+        blogsRepositoryDb.blogs.push(newBlog);
+        return newBlog;
+    },
+    updateBlog(id: number, name: string, description: string, websiteUrl: string) {
+        const foundBlogsUpdate = blogsRepositoryDb.blogs.find(b => b.id === id);
+        if(foundBlogsUpdate) {
+            foundBlogsUpdate.name = name;
+            foundBlogsUpdate.description = description;
+            foundBlogsUpdate.websiteUrl = websiteUrl;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 }
