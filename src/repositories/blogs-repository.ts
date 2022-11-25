2423442ab80f394ce7ec850af85e028ced35806f
illuhs <illuhs@yandex.ru>
2022-11-25 22:10:22 +0300

    refactoring blogs-repository.ts

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -1,48 +1,24 @@
 import {blogsCollection, BlogsType} from "./db";
 import {ObjectId} from "mongodb";
 
-
-type BlogsRepositoryType = {
-    blogs: BlogsType[];
-};
-const blogsRepositoryDb: BlogsRepositoryType = {
-    blogs: []
-};
-
 export const blogsRepository = {       //объект с методами управления данными
     async returnAllBlogs () {
-        return blogsCollection.find().toArray();//blogsRepositoryDb.blogs;
+        return blogsCollection.find().toArray();
     },
     async findBlogById(id: string): Promise<BlogsType | null> {
-        return blogsCollection.findOne({_id: new ObjectId(id)}) //blogsRepositoryDb.blogs.find(b => b.id === id); //array || undefined
+        return blogsCollection.findOne({_id: new ObjectId(id)})  //object || undefined
     },
     async deleteBlogByTd(id: string) {
         return (await blogsCollection.deleteOne({_id: new ObjectId(id)})).deletedCount === 1;
-        /*for(let i = 0; i < blogsRepositoryDb.blogs.length; i++) {
-            if(blogsRepositoryDb.blogs[i].id === id) {
-                blogsRepositoryDb.blogs.splice(i, 1);
-                return true;
-            }
-        }
-        return false*/
     },
     async createBlog(name: string, description: string, websiteUrl: string) {
         const result = await blogsCollection.insertOne({
-            //id: String((new Date()).valueOf()),
             name: name,
             description: description,
             websiteUrl: websiteUrl,
             createdAt: new Date().toISOString()
         });
         return blogsRepository.findBlogById(result.insertedId.toString());
-        /*const newBlog = {
-            id: String((new Date()).valueOf()),
-            name: name,
-            description: description,
-            websiteUrl: websiteUrl
-        };
-        blogsRepositoryDb.blogs.push(newBlog);
-        return newBlog;*/
     },
     async updateBlog(id: string, name: string, description: string, websiteUrl: string) {
         return (await blogsCollection.updateOne({_id: new ObjectId(id)}, { $set: {
@@ -50,19 +26,8 @@ export const blogsRepository = {       //объект с методами упр
             description: description,
             websiteUrl: websiteUrl
         }})).matchedCount === 1;
-        /*const foundBlogsUpdate = blogsRepositoryDb.blogs.find(b => b.id === id);
-        if(foundBlogsUpdate) {
-            foundBlogsUpdate.name = name;
-            foundBlogsUpdate.description = description;
-            foundBlogsUpdate.websiteUrl = websiteUrl;
-            return true;
-        }
-        else {
-            return false;
-        }*/
     },
     async allBlogsDelete() {
         await blogsCollection.deleteMany({})
-        //blogsRepositoryDb.blogs = [];
     }
 }
