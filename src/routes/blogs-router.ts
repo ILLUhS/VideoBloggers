762eb5d372c8c15b7b673f102957e401d3d73641
illuhs <illuhs@yandex.ru>
2022-12-01 00:53:41 +0300

    add method post of blogs/:id/post

--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -1,12 +1,18 @@
 import {Request, Response, Router} from "express";
 import {authorizationGuardMiddleware} from "../middlewares/authorization-guard-middleware";
 import {
-        blogIdPostValidation,
-        descriptionBlogValidation, errorsValidation,
-        nameBlogValidation, queryParamsValidation, websiteUrlBlogValidation
+        contentPostValidation,
+        descriptionBlogValidation,
+        errorsValidation,
+        nameBlogValidation,
+        queryParamsValidation,
+        shortDescriptionPostValidation,
+        titlePostValidation,
+        websiteUrlBlogValidation
 } from "../middlewares/input-validation-middleware";
 import {queryRepository} from "../repositories/query-repository";
 import {blogsService} from "../services/blogs-service";
+import {postsService} from "../services/posts-service";
 
 export const blogsRouter = Router({});
 
@@ -23,10 +29,14 @@ blogsRouter.get('/:id', async (req, res) => {
                 return res.sendStatus(404);
         }
 })
-blogsRouter.get('/:id/posts', blogIdPostValidation, async (req: Request, res: Response) => {
-        const searchParams = queryParamsValidation(req.query);
-        return res.status(200).json(await queryRepository.getPotsWithQueryParamAndBlogId(searchParams,
-            String(req.params.id)));
+blogsRouter.get('/:id/posts', async (req: Request, res: Response) => {
+        const foundBlog = await queryRepository.findBlogById(String(req.params.id));
+        if(foundBlog) {
+                const searchParams = queryParamsValidation(req.query);
+                return res.status(200).json(await queryRepository.getPotsWithQueryParamAndBlogId(searchParams,
+                    String(req.params.id)));
+        }
+        return res.status(404).send('If specified blog doesn\'t exists')
 })
 blogsRouter.delete('/:id', authorizationGuardMiddleware, async (req, res) => {
         const deletedBlog = await blogsService.deleteBlogByTd(String(req.params.id));
@@ -44,6 +54,21 @@ blogsRouter.post('/', authorizationGuardMiddleware, nameBlogValidation,
             String(req.body.description), String(req.body.websiteUrl));
         return res.status(201).json(createdBlog)
 })
+blogsRouter.post('/:id/posts', authorizationGuardMiddleware,
+    titlePostValidation, shortDescriptionPostValidation,
+    contentPostValidation, errorsValidation,
+    async (req: Request, res: Response) => {
+            const foundBlog = await queryRepository.findBlogById(String(req.params.id));
+            if(foundBlog) {
+                    const createdPost = await postsService.createPost(
+                        String(req.body.title),
+                        String(req.body.shortDescription),
+                        String(req.body.content), String(req.body.blogId)
+                    );
+                    return res.status(201).json(createdPost)
+            }
+            return res.status(404).send('If specified blog doesn\'t exists')
+    })
 blogsRouter.put('/:id', authorizationGuardMiddleware, nameBlogValidation,
     descriptionBlogValidation, websiteUrlBlogValidation, errorsValidation,
     async (req: Request, res: Response) => {
