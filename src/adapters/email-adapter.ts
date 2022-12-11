b5105b5449dca407fd50e5a2c1fe2233743ee55e
illuhs <illuhs@yandex.ru>
2022-12-11 22:22:31 +0300

    add send email user

---
+++ src/adapters/email-adapter.ts
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+import {settings} from "../config/settings";
+export const emailAdapter = {
+    async sendEmail(email: string, subject: string, message: string) {
+        let transporter = nodemailer.createTransport({
+            service: "DebugMail",
+            auth: {
+                user: settings.EMAIL,
+                pass: settings.EMAIL_PASS
+            },
+        });
+
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: '"Illuhs Team" <centfrost@protonmail.com>', // sender address
+            to: email, // list of receivers
+            subject: subject, // Subject line тема емейла
+            html: message, // html body
+        });
+    }
+}
--- src/config/settings.ts
+++ src/config/settings.ts
@@ -3,5 +3,7 @@ dotenv.config();
 export const settings = {
     MONGO_URL: process.env.MONGO_URL || "mongodb://127.0.0.1:27017",
     JWT_SECRET: process.env.JWT_SECRET || "123",
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL: process.env.EMAIL
 }
