077c856be0c6ff76686589d3622ea1d18f3d94b5
illuhs <illuhs@yandex.ru>
2023-01-06 14:57:19 +0300

    user-repository go to mongoose

--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -1,11 +1,22 @@
 import {UserCreateModel} from "../types/models/user-create-model";
 import {UserModel, usersCollection} from "./db";
 import {UserViewModel} from "../types/models/user-view-model";
-
+type filter = {
+    [filed: string]: string;
+};
+type update = {
+    [filed: string]: string;
+}
 export const usersRepository = {
-    async create(newUser: UserCreateModel) {
-        await UserModel.create(newUser);
-        return true;
+    async create(newUser: UserCreateModel): Promise<boolean> {
+        try {
+            await UserModel.create(newUser);
+            return true;
+        }
+        catch (e) {
+            console.log(e);
+            return false
+        }
 
 
         //return (await usersCollection.insertOne({...newUser})).acknowledged;
@@ -41,42 +52,62 @@ export const usersRepository = {
         } : null;*/
     },
     async findByFieldWithHash(field: string, value: string) {
-        return await usersCollection.findOne({[field]: value}, {projection: {_id: 0}});
+        return await UserModel.findOne({[field]: value}).select({_id: 0, __v: 0}).exec();
+        //return await usersCollection.findOne({[field]: value}, {projection: {_id: 0}});
     },
     async deleteById(id: string) {
-        return (await usersCollection.deleteOne({id: id})).deletedCount === 1;
+        return (await UserModel.deleteOne({id: id}).exec()).deletedCount === 1;
+        //return (await usersCollection.deleteOne({id: id})).deletedCount === 1;
     },
     async deleteAll() {
-        return (await usersCollection.deleteMany({})).acknowledged;
+        return (await UserModel.deleteMany().exec()).acknowledged;
+        //return (await usersCollection.deleteMany({})).acknowledged;
     },
     async updateConfirmation(id: string): Promise<boolean> {
-        return (await usersCollection.updateOne({id: id},
-            {$set: {'emailConfirmation.isConfirmed': true}})).matchedCount === 1;
+        return (await UserModel.updateOne({id: id},
+            {'emailConfirmation.isConfirmed': true}).exec()).matchedCount === 1;
+        /*return (await usersCollection.updateOne({id: id},
+            {$set: {'emailConfirmation.isConfirmed': true}})).matchedCount === 1;*/
     },
     async updateExpirationTime(id: string, expirationTime: Date): Promise<boolean> {
-        return (await usersCollection.updateOne({id: id},
-            {$set: {'emailConfirmation.expirationTime': expirationTime}})).matchedCount === 1;
+        return (await UserModel.updateOne({id: id},
+            {'emailConfirmation.expirationTime': expirationTime}).exec()).matchedCount === 1;
+        /*return (await usersCollection.updateOne({id: id},
+            {$set: {'emailConfirmation.expirationTime': expirationTime}})).matchedCount === 1;*/
     },
     async updateConfirmationCode(id: string, code: string): Promise<boolean> {
-        return (await usersCollection.updateOne({id: id},
-            {$set: {'emailConfirmation.confirmationCode': code}})).matchedCount === 1;
+        return (await UserModel.updateOne({id: id},
+            {'emailConfirmation.confirmationCode': code}).exec()).matchedCount === 1;
+        /*return (await usersCollection.updateOne({id: id},
+            {$set: {'emailConfirmation.confirmationCode': code}})).matchedCount === 1;*/
     },
-    async updatePassRecovery(id: string, code: string, expiration: Date, isConfirmed: boolean) {
-        return (await usersCollection.updateOne({id: id},
+    async updatePassRecovery(id: string, code: string, expiration: Date, isConfirmed: boolean): Promise<boolean> {
+        return (await UserModel.updateOne({id: id},
+            {
+                'passwordRecovery.recoveryCode': code,
+                'passwordRecovery.expirationTime': expiration,
+                'passwordRecovery.isUsed': isConfirmed
+            }).exec()).matchedCount === 1;
+        /*return (await usersCollection.updateOne({id: id},
             {
                 $set: {
                     'passwordRecovery.recoveryCode': code,
                     'passwordRecovery.expirationTime': expiration,
                     'passwordRecovery.isUsed': isConfirmed
                 }
-            })).matchedCount === 1;
+            })).matchedCount === 1;*/
     },
     async updatePassRecoveryStatus(id: string): Promise<boolean> {
         return (await usersCollection.updateOne({id: id},
             {$set: {'passwordRecovery.isUsed': true}})).matchedCount === 1;
     },
     async updatePassword(id: string, newPasswordHash: string): Promise<boolean> {
+        await this.updateOneField({'id': id}, {'newPasswordHash': newPasswordHash})
         return (await usersCollection.updateOne({id: id},
             {$set: {'accountData.passwordHash': newPasswordHash}})).matchedCount === 1;
+    },
+    async updateOneField(filter: filter, update: update) {
+        return (await UserModel.updateOne(filter,
+            update).exec()).matchedCount === 1;
     }
 }
